0275edd4b35af74ce840ab578e486cc87f23cec1
travishw <191087@trinityschools.nz>
2023-02-16 17:34:17 +1300

    Testing github commit works

--- src/main/java/ttav/modid/ttavMod.java
+++ src/main/java/ttav/modid/ttavMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// very important comment
 public class ttavMod implements ModInitializer {
 	public static final String MOD_ID = "ttavmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("ttavmod");
