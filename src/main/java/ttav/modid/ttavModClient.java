fcd6a52276ffee9ca50f63e6ec955da499537a3d
travishw <191087@trinityschools.nz>
2023-02-16 17:30:50 +1300

    Created Project

---
+++ src/main/java/ttav/modid/ttavModClient.java
@@ -0,0 +1,10 @@
+package ttav.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ttavModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
