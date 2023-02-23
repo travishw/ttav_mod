a44deef2c2ce8b2f5f11578a59f6aa26b9b99765
travishw <191087@trinityschools.nz>
2023-02-24 09:49:18 +1300

    Added Refined Aesir Item. Uses temporary texture

--- src/main/java/ttav/modid/ttavMod.java
+++ src/main/java/ttav/modid/ttavMod.java
@@ -4,17 +4,14 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-// very important comment
+import ttav.modid.item.ModItems;
+
 public class ttavMod implements ModInitializer {
 	public static final String MOD_ID = "ttavmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("ttavmod");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
